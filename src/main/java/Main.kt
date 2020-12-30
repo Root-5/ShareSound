8e08a48e2133a3971efde05d0c102681a458a745
Ruby_Thrash <mr.safik322@gmail.com>
2020-12-30 18:13:40 +0700

    Initial commit

---
+++ src/main/java/Main.kt
@@ -0,0 +1,9 @@
+import javafx.application.Application
+import javafx.scene.Group
+import javafx.scene.Scene
+
+fun main(args: Array<String>) {
+    val window = Window()
+    window.launch()
+
+}
---
+++ src/main/java/Window.kt
@@ -0,0 +1,20 @@
+import javafx.application.Application
+import javafx.scene.Group
+import javafx.scene.Scene
+import javafx.stage.Stage
+
+class Window() : Application() {
+
+    fun launch() {
+        Application.launch()
+    }
+
+    override fun start(p0: Stage) {
+        val group = Group()
+        val scene = Scene(group)
+
+        p0.title = "ShareSoundApp"
+        p0.scene = scene
+        p0.show()
+    }
+}
